4d992f129276ddf791d10ff77c17113998882f61
huangfusuper <huangfusuper@163.com>
2020-09-27 17:35:22 +0800

    全局启动引导开发

---
+++ src/main/java/com/apm/processors/SystemApmBootstrap.java
@@ -0,0 +1,71 @@
+package com.apm.processors;
+
+import com.apm.inits.BaseCollectionInitialization;
+import com.apm.trans.BaseClassFileTransformer;
+import com.apm.trans.wrappers.ClassFileTransformerWrappers;
+
+import java.lang.instrument.Instrumentation;
+import java.util.List;
+
+/**
+ * 系统监控启动引导
+ *
+ * @author huangfu
+ */
+public class SystemApmBootstrap {
+    /**
+     * 全局仪表监控管理器
+     */
+    private final Instrumentation instrumentation;
+    /**
+     * 参数
+     */
+    private final String args;
+
+    private BaseCollectionInitialization baseCollectionInitialization;
+
+    /**
+     * 系统监控
+     *
+     * @param instrumentation 监控管理器
+     * @param args            参数
+     */
+    public SystemApmBootstrap(Instrumentation instrumentation, String args) {
+        this.instrumentation = instrumentation;
+        this.args = args;
+    }
+
+    /**
+     * 处理器
+     *
+     * @param baseCollectionInitialization 统计初始化程序
+     * @return 当前对象
+     */
+    public SystemApmBootstrap handler(BaseCollectionInitialization baseCollectionInitialization) {
+        baseCollectionInitialization.doInitialization(instrumentation, args);
+        this.baseCollectionInitialization = baseCollectionInitialization;
+        return this;
+    }
+
+    /**
+     * 启动
+     *
+     * @return 启动引导
+     */
+    public SystemApmBootstrap starter() {
+        List<ClassFileTransformerWrappers> classFileTransformerWrapperList = this.baseCollectionInitialization.getClassFileTransformerWrapperList();
+        classFileTransformerWrapperList.forEach(classFileTransformerWrappers -> {
+            BaseClassFileTransformer classFileTransformer = classFileTransformerWrappers.getClassFileTransformer();
+            this.instrumentation.addTransformer((loader, className, classBeingRedefined, protectionDomain, classfileBuffer) -> {
+                className = className.replaceAll("/",".");
+                //是否匹配预设规则
+                if (classFileTransformer.dataMatching(className)) {
+                    //调用处理程序
+                    return classFileTransformer.doTransform(loader, className, classBeingRedefined, protectionDomain, classfileBuffer);
+                }
+                return null;
+            });
+        });
+        return this;
+    }
+}
